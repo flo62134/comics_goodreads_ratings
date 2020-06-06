47c79c3a7c219f6a263b3bef3d5fcfb611e977be
flo62134 <florentbrassart31@gmail.com>
2020-06-06 18:57:52 +0200

    Basic version - Created empty functions and initialized tests

---
+++ comics_goodreads_ratings.py
@@ -0,0 +1,32 @@
+import config
+
+GOODREADS_SEARCH_URL = 'https://www.goodreads.com/search.xml'
+
+
+def get_book_query(book):
+    pass
+
+
+def search_book(book):
+    pass
+
+
+def fetch_book_rating(book: str):
+    pass
+
+
+def ask_books():
+    json = input('Enter books response from Comixology:')
+    return dict(json)
+
+
+def display_ratings(books):
+    pass
+
+
+def main():
+    print('Hello world!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_comics_goodreads_ratings.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_get_book_query(self):
+        self.fail()
+
+    def test_search_book(self):
+        self.fail()
+
+    def test_fetch_book_rating(self):
+        self.fail()
+
+    def test_ask_books(self):
+        self.fail()
+
+    def test_display_ratings(self):
+        self.fail()
