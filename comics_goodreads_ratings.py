ed921f4323492546240c3c5bd584a69872b06c59
flo62134 <florentbrassart31@gmail.com>
2020-06-13 13:55:22 +0200

    Basic version - Fetch good on goodreads

--- comics_goodreads_ratings.py
+++ comics_goodreads_ratings.py
@@ -1,11 +1,9 @@
 import config
 import json
+import urllib.parse
+import requests
 
 GOODREADS_SEARCH_URL = 'https://www.goodreads.com/search.xml'
-
-
-def get_goodreads_query(book):
-    pass
 
 
 def get_goodreads_keyword(book):
@@ -23,8 +21,16 @@ def get_goodreads_keyword(book):
         return f'{title}'
 
 
+def get_goodreads_search_url(book):
+    keyword = get_goodreads_keyword(book)
+    query = urllib.parse.quote(keyword)
+    return f'{GOODREADS_SEARCH_URL}?key={config.GOODREADS_API_KEY}&q={query}'
+
+
 def search_goodreads_book(book):
-    pass
+    url = get_goodreads_search_url(book)
+    goodreads_book = requests.get(url)
+    return goodreads_book.text
 
 
 def fetch_book_rating(book: str):
@@ -33,7 +39,7 @@ def fetch_book_rating(book: str):
 
 def read_books(file_path: str):
     with open(file_path) as json_file:
-        return json.load(json_file)
+        return json.load(json_file)['objects']
 
 
 def display_ratings(books):
@@ -42,6 +48,7 @@ def display_ratings(books):
 
 def main():
     books = read_books('./comixology_books.json')
+    print(search_goodreads_book(books[0]))
 
 
 if __name__ == "__main__":
